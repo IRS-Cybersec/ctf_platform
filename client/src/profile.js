06a583b75f243a87131b0a15fab327fbeb7007ce
Leonard <leothepro555@gmail.com>
2020-06-04 23:06:29 +0800

    Added pie chart to Profile Page
    I hate myself.

--- client/src/profile.js
+++ client/src/profile.js
@@ -1,66 +1,57 @@
 import React from 'react';
 import { Layout, Menu, Breadcrumb, Typography, Empty, Divider, Avatar } from 'antd';
-import {
-    FlagTwoTone,
-    HomeTwoTone,
-    FundTwoTone,
-    NotificationTwoTone,
-    SmileTwoTone,
-    AimOutlined
-} from '@ant-design/icons';
+import {PieChart, Pie, Cell, Tooltip} from 'recharts';
 import './App.css';
 import { NavLink, Switch, Route, withRouter, useHistory, useLocation } from 'react-router-dom';
 
+
 const { Header, Content, Footer, Sider } = Layout;
 const { Title, Paragraph, Text } = Typography;
-
 class Profile extends React.Component {
+    
 
     constructor(props) {
         super(props);
-        /*      fetch('https://api.irscybersec.tk/v1/account/list', {
-                    method: 'get',
-                    headers: { 'Content-Type': 'application/json', 'Authorization': localStorage.getItem('token') },
-                }).then((results) => {
-                    return results.json(); //return data in JSON (since its JSON data)
-                }).then((data) => {
-        
-                    if (data.success === true) {
-                        const retrievedData = data.explore.questions
-                        const currentData = this.state.explorePostsData
-                        this.setState({ explorePostsData: currentData.concat(retrievedData) }) //Concat newly retrieved data
-                        this.setState({ loading: false, }) //Done loading, set loading state to false
-        
-                        console.log(data.explore.questions)
-                    }
-                }).catch((error) => {
-                   console.log(error);
-                })  */
 
         this.state = {
-            username: "Tkaixiang",
-            email: "tkaixiang@gmail.com",
-            type: 1,
-            score: 1999
+            targetUser: this.props.username,
+            score: 350,
+            solved: [
+                {
+                  type: 'pwn',
+                  value: 100,
+                },
+                {
+                  type: 'web',
+                  value: 200,
+                },
+                {
+                  type: 'forensics',
+                  value: 50,
+                },
+                {
+                  type: 'crypto',
+                  value: 50,
+                },
+              ]
         };
 
-        var targetUser = this.state.username;
 
         var path = window.location.pathname.split("/");
         console.log(path.length);
         if (path.length == 3) {
-            targetUser = path[2];
-            if (targetUser != "")
+            this.state.targetUser = path[2];
+            if (this.state.targetUser != "")
                 //Just a temp check. Replace with a db call of some kind.
-                if (targetUser != "Tkaixiang") {
-                    this.state.username = null;
+                if (this.state.targetUser != "Tkaixiang") {
+                    this.state.targetUser = null;
                 }
         }
     }
 
     render() {
         //Empty page. No user found
-        if (this.state.username == null) {
+        if (this.state.targetUser == null) {
             return (
                 <Layout style={{ height: "100%", width: "100%" }}>
                     <br /><br /><br />
@@ -69,33 +60,84 @@ class Profile extends React.Component {
             );
         }
 
+        //User found, show general details.
         var header = (
             <div style={{display: "flex", justifyContent: "row", alignContent: "center", alignItems: "center", float: "right"}}>
                 <Avatar style={{backgroundColor:"Red",marginRight: "3%"}} size={100} src="https://www.todayifoundout.com/wp-content/uploads/2017/11/rick-astley.png"/>
-                <Title style={{backgroundColor:"blue"}}>{this.props.username}</Title>
+                <Title>{this.state.targetUser}</Title>
+            </div>
+        );
+
+        const RADIAN = Math.PI / 180;  
+        const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius,value,type }) => {
+            const radius = innerRadius + (outerRadius - innerRadius) * 1.2;
+         const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+         const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+            var tag = "";
+            if(value > 0){
+                tag = type;
+            }
+         return (
+           <text style={{fontWeight: "bolder"}}x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
+               {tag}
+           </text>
+         );
+       };
+
+        const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+        var chart = (
+            <div style={{width: "40%",height:"40%", marginRight:"3%",marginBottom:"3%"}}>
+                <div style={{display: "flex",alignContent: "center", alignItems: "center"}}>
+                    
+                    <PieChart width={800} height={350} onMouseEnter={this.onPieEnter}>
+                        <Pie
+                        data={this.state.solved}
+                        cx={250}
+                        cy={150}
+                        outerRadius={100}
+                        fill="#8884d8"
+                        label={renderCustomizedLabel}
+                        labelLine={false}
+                        paddingAngle={0}
+                        dataKey="value"
+                        >
+                        {
+                            this.state.solved.map((entry, index) => 
+                            <Cell key={`${index}`} fill={COLORS[index % COLORS.length]} 
+                            />)
+                        }
+                        </Pie>
+                    </PieChart>
+                </div>
+                <Divider plain />
             </div>
         );
 
         var score = (
             <div style={{width: "40%",height:"40%", marginRight:"3%",marginBottom:"3%"}}>
                 <div style={{display: "flex",alignContent: "center", alignItems: "center"}}>
-                    <Title style={{marginRight:"1vw",fontSize:"3vw"}}>{this.state.score}</Title>
-                    <AimOutlined style={{fontSize:"3vw"}}/>
+                    <Title style={{marginRight:"1vw",fontSize:"3vw",textAlign:"center"}}>{this.state.score+"⌖"}</Title>
+                    <div style={{width:800,height:350}}></div>
                 </div>
                 <Divider plain />
             </div>
         );
+
+        //If the user is the same as your account, show more info.
+
         return (
             <Layout style={{ height: "100%", width: "100%", padding: "3%" }}>
                 {header}
                 <Divider plain />
                 <div style={{display: "flex",justifyContent: "row"}}>
                     {score}
-                    {score}
+                    {chart}
                 </div>
             </Layout>
         );
     }
 }
+
 
 export default Profile;
