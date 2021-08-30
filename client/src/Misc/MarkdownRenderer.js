b756274cac2b5022811205b1c343c0ab0dce52c5
Tkaixiang <tkaixiang@gmail.com>
2021-08-31 00:42:01 +0800

    removed math support due to low usage

--- client/src/Misc/MarkdownRenderer.js
+++ client/src/Misc/MarkdownRenderer.js
@@ -1,6 +1,4 @@
 import ReactMarkdown from 'react-markdown';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
 import rehypeRaw from 'rehype-raw';
 import 'katex/dist/katex.min.css';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
@@ -23,7 +21,7 @@ function MarkdownRender(props) {
     //rehypeRaw is to render HTML to support older challenge descriptions which were written in JSX
     //should be removed in the future
     return (
-        <ReactMarkdown remarkPlugins={[remarkMath, gfm]} rehypePlugins={[rehypeKatex, rehypeRaw]} components={components} children={props.children} />
+        <ReactMarkdown remarkPlugins={[gfm]} rehypePlugins={[rehypeRaw]} components={components} children={props.children} />
     );
 }
 
