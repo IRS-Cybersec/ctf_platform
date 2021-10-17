be4ee68e99308891d88d4cb3a6b1da7c428a9cbe
Tkaixiang <tkaixiang@gmail.com>
2021-10-17 13:43:50 +0800

    fix "team score" msg when teams are disabled

--- api/controllers/Teams.js
+++ api/controllers/Teams.js
@@ -65,8 +65,7 @@ const get = async (req, res) => {
 // get the team the user is in for the sidebar
 const userTeam = (req, res) => {
     const usernameTeamCache = NodeCacheObj.get("usernameTeamCache")
-    if (NodeCacheObj.get("teamMode")) {
-        if (req.locals.username in usernameTeamCache) {
+        if (NodeCacheObj.get("teamMode") && req.locals.username in usernameTeamCache) {
             res.send({
                 success: true,
                 team: usernameTeamCache[req.locals.username]
@@ -78,12 +77,6 @@ const userTeam = (req, res) => {
                 team: false
             })
         }
-
-    }
-    else res.send({
-        success: false,
-        error: "teams-disabled"
-    })
 }
 
 const linkInfo = async (req, res) => {
